6d48e2e05d2af5e8e97933df88defb9cb8461357
Ishan Pandey <70710170+ishanpan@users.noreply.github.com>
2023-03-10 20:23:56 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.cpp
+++ Number of Provinces - GFG/number-of-provinces.cpp
@@ -9,42 +9,61 @@ using namespace std;
 class Solution {
   public:
   
- 
-  void dfs(vector<int>adjL[], int V, int visited[], int i){
-      visited[i] = 1;
-      for(auto it:adjL[i]){
-          if(!visited[it]){
-              dfs(adjL,V,visited,it);
-          }
+  void unionn(vector<int>&parent, vector<int>&rank,int u, int v){
+      
+      int parentU = getParent(parent,u);
+      int parentV = getParent(parent,v);
+      if(parentU==parentV){
+          return;
       }
+      
+      if(rank[parentU]<rank[parentV]){
+          parent[parentU] = parentV;
+      }
+      else if(rank[parentU]>rank[parentV]){
+          parent[parentV] = parentU;
+      }
+      else{
+          parent[parentV] = parentU;
+          rank[parentU]++;
+      }
+      
+  }
+  
+  int getParent(vector<int>&parent, int u){
+      
+      if(parent[u]==u){
+          return u;
+      }
+      
+      return parent[u] = getParent(parent, parent[u]);
       
   }
   
     int numProvinces(vector<vector<int>> adj, int V) {
         // code here
-        int visited[V+1] = {0};
-        int ans = 0;
-        
-        vector<int>adjL[V];
+        //union every possible vertex;
+        vector<int>parent(V);
+        for(int i=0;i<V;i++){
+            parent[i]=i;
+        }
+        vector<int>rank(V,0);
         for(int i=0;i<V;i++){
             for(int j=0;j<V;j++){
-                if(adj[i][j]==1 && i!=j){
-                    adjL[i].push_back(j);
-                    adjL[j].push_back(i);
+                 if(adj[i][j]==1){
+                   unionn(parent,rank,i,j); 
                 }
+                
             }
         }
         
-
+        int cnt = 0;
         for(int i=0;i<V;i++){
-            if(!visited[i]){
-                ans++;
-                dfs(adjL,V,visited,i);
+            if(getParent(parent,i)==i){
+                cnt++;
             }
         }
-        return ans;
-        
-        
+        return cnt;
         
     }
 };
