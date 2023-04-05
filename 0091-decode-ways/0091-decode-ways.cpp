b2446c3734c9fafe82fad959517d9ec80e5dd394
Ishan Pandey <70710170+ishanpan@users.noreply.github.com>
2023-04-05 16:38:19 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (95.14%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,38 +1,59 @@
 class Solution {
 public:
     
-    int t[101];
-    int numsS(string s, int sizes, int i){
+
+//     int numsS(string s, int sizes, int i){
         
-        if(t[i]!=-1){
-            return t[i];
-        }
-        if(i==sizes){
-            return 1;
-        }
-        if(s[i]=='0'){
-            return 0;
-        }
-        t[i] = numsS(s,sizes,i+1);
-        if(i<sizes-1 && (s[i]=='1'|| (s[i]=='2' && s[i+1]<'7'))){
-            t[i] += numsS(s,sizes,i+2);
-        }
+//         if(t[i]!=-1){
+//             return t[i];
+//         }
+//         if(i==sizes){
+//             return 1;
+//         }
+//         if(s[i]=='0'){
+//             return 0;
+//         }
+//         t[i] = numsS(s,sizes,i+1);
+//         if(i<sizes-1 && (s[i]=='1'|| (s[i]=='2' && s[i+1]<'7'))){
+//             t[i] += numsS(s,sizes,i+2);
+//         }
         
-        return t[i];
+//         return t[i];
         
 
-    }
+//     }
     
     
     int numDecodings(string s) {
+        int n = s.size();
+        int t[n+1];
         
-        
-        
-        if(s.size()==0){
-            return 0;
+        for(int i=0;i<n+1;i++){
+            t[i] = 0;
         }
-        memset(t,-1,sizeof(t));
-        return numsS(s,s.size(),0);
+        t[0]=1;
+        for(int i=1;i<n+1;i++){
+            
+            
+            if(s[i-1]>='1' && s[i-1]<='9'){
+                t[i] +=t[i-1];
+            }
+            if(i==1){
+                continue;
+            }
+            
+             if((s[i-2]=='1'|| (s[i-2]=='2' && s[i-1]<'7'))){
+                t[i] +=t[i-2];
+            }
+            
+        }
+        
+        return t[n];
+        
+        
+        
+        
+      
         
     }
 };
