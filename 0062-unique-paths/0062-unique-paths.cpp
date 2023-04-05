cf0c87ac4fccfbc5ea69502514c0fb923e1063b3
Ishan Pandey <70710170+ishanpan@users.noreply.github.com>
2023-04-05 17:06:48 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (98.39%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,38 +2,51 @@ class Solution {
 public:
     
     int t[101][101];
-    bool valid(int m,int n,int i,int j){
-        if(i>=0 && j>=0 && i<m && j<n){
-            return true;
-        }
-        return false;
-        
-    }
+
     int recur(int m,int n,int i,int j){
-        
         if(t[i][j]!=-1){
             return t[i][j];
         }
         if(!(i>=0 && j>=0 && i<m && j<n)){
             return 0;
         }
-        
         if(i==m-1 && j==n-1){
             return 1;
         }
-        
-        
-        t[i][j]=recur(m,n,i+1,j);
-        
-         t[i][j] += recur(m,n,i,j+1);
+
+        t[i][j]=recur(m,n,i+1,j)+recur(m,n,i,j+1);
         
         return t[i][j];
-        
     }
     
     int uniquePaths(int m, int n) {
-        memset(t,-1,sizeof(t));
-        return recur(m,n,0,0);
+        //tabulation only one way reach cell[0][0] and cell[i][0] and cell[0][j];
+        
+        int t[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0){
+                    t[i][j] = 1;
+                }
+                if(j==0){
+                    t[i][j] = 1;
+                }
+            }
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                t[i][j] = t[i-1][j] + t[i][j-1];
+            }
+        }
+        
+        return t[m-1][n-1];
+        
+        
+        
+        
+        
+        
         
         
     }
