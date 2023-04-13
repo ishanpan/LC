77e9bfbf51ec39be9b14f3c9330f5f17c88269a9
Ishan Pandey <70710170+ishanpan@users.noreply.github.com>
2023-04-13 23:37:51 +0530

    Time: 2 ms (63.39%), Space: 7.1 MB (77.30%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
-    
-    int dp[1001][2];
-    int wigglewiggle(int i, int n, bool flag, vector<int>arr){
-        if(i==n-1){
-            return 1;
-        }
-        if(dp[i][flag]!=-1){
-            return dp[i][flag];
-        }
-        int ans = 0;
-        if(flag){
-            if(arr[i+1]>arr[i]){
-                ans = max(ans,1+wigglewiggle(i+1,n,false,arr));
-            }
-            else{
-                ans = max(ans,wigglewiggle(i+1,n,true,arr));
-            }
-        }
-        else{
-            if(arr[i+1]<arr[i]){
-                ans = max(ans,1+wigglewiggle(i+1,n,true,arr));
-            }
-            else{
-                ans = max(ans,wigglewiggle(i+1,n,false,arr));
-            }
-        }
-        return dp[i][flag]=ans;
-    }
-    
-    
+    // int dp[1001][2];
+    // int wigglewiggle(int i, int n, bool flag, vector<int>arr){
+    //     if(i==n-1){
+    //         return 1;
+    //     }
+    //     if(dp[i][flag]!=-1){
+    //         return dp[i][flag];
+    //     }
+    //     int ans = 0;
+    //     if(flag){
+    //         if(arr[i+1]>arr[i]){
+    //             ans = max(ans,1+wigglewiggle(i+1,n,false,arr));
+    //         }
+    //         else{
+    //             ans = max(ans,wigglewiggle(i+1,n,true,arr));
+    //         }
+    //     }
+    //     else{
+    //         if(arr[i+1]<arr[i]){
+    //             ans = max(ans,1+wigglewiggle(i+1,n,true,arr));
+    //         }
+    //         else{
+    //             ans = max(ans,wigglewiggle(i+1,n,false,arr));
+    //         }
+    //     }
+    //     return dp[i][flag]=ans;
+    // }
+
     int wiggleMaxLength(vector<int>& nums) {
-     
-        memset(dp,-1,sizeof(dp));
-        
-        int posi = wigglewiggle(0,nums.size(),true,nums);
-        int negi = wigglewiggle(0,nums.size(),false,nums);
+        int posi = 1;
+        int negi = 1;
+        int n = nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1]){
+                posi = 1+negi;
+            }
+            else if(nums[i]<nums[i-1]){
+                negi = 1+posi;
+            }
+        }
         return max(posi,negi);
-        
-        
     }
 };
