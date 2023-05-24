1c65adf5610f43b88099ab6438fe38908834827d
Ishan Pandey <70710170+ishanpan@users.noreply.github.com>
2023-05-24 18:45:07 +0530

    Time: 7 ms (51.08%), Space: 16.4 MB (48.38%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -17,29 +17,55 @@ public:
         if(root1==NULL && root2==NULL){
             return true;
         }
-        
         if(root1==NULL || root2==NULL || root1->val!=root2->val){
             return false;
         }
         
-       
         bool ch1 = true;
         ch1 = checkLR(root1->left,root2->right);
         bool ch2 = true;
-        ch2 = checkLR(root1->right,root2->left);
-        
+        ch2 = checkLR(root1->right,root2->left);        
         return (ch1 && ch2);
-        
-        
+
     }
     
     
     bool isSymmetric(TreeNode* root) {
-      
-        if(root==NULL){
+        
+        if(root->left==NULL && root->right==NULL){
             return true;
         }
-        return checkLR(root->left,root->right);
+        queue<TreeNode*>q;
+        q.push(root->left);
+        q.push(root->right);
+        while(!q.empty()){
+          
+            TreeNode* q1 = q.front();
+            q.pop();
+            TreeNode* q2;
+            if(q.size()>0){
+                q2 = q.front(); 
+                q.pop();
+            }
+            if(q1==NULL && q2==NULL){
+                continue;
+            }
+            if(q1==NULL || q2==NULL || q1->val!=q2->val )
+            {
+                return false;
+            }
+            
+                q.push(q1->left);
+                q.push(q2->right);
+            
+             
+                q.push(q1->right);
+                q.push(q2->left);
+            
+        }
+        
+        return true;
+        
         
         
     }
