91be7ef326a0733025cc4b02d8c3160b8d86d8bf
Ishan Pandey <70710170+ishanpan@users.noreply.github.com>
2023-06-07 22:28:27 +0530

    Added solution - LeetHub

--- Find position of set bit - GFG/find-position-of-set-bit.cpp
+++ Find position of set bit - GFG/find-position-of-set-bit.cpp
@@ -7,23 +7,25 @@ class Solution {
   public:
     int findPosition(int N) {
         // code here
-        int count = 0;
-       int pos = 0;
-       while(N){
-           if(N & 1){
-               count++;
-           }
-           N = N>>1;
-           pos++;
-       }
-        if(count==1){
-            return pos;
-        }
-        else{
-            return -1;
-        }
         
-
+        int pos = 1;
+        int ans = -1;
+        bool Nfound = true;
+        while(N!=0){
+            
+            if((N&1)==1 && Nfound){
+                ans = pos;
+                Nfound = false;
+            }
+            else if((N&1)==1 && !Nfound){
+                return -1;
+            }
+            pos++;
+            N>>=1;
+            
+        }
+        return ans;
+        
     }
 };
 
