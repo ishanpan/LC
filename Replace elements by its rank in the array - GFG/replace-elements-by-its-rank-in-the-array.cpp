4828360ec14414967e3c037bb5b35c736e95ec58
Ishan Pandey <70710170+ishanpan@users.noreply.github.com>
2023-06-13 04:00:11 +0530

    Added solution - LeetHub

---
+++ Replace elements by its rank in the array - GFG/replace-elements-by-its-rank-in-the-array.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+
+    vector<int> replaceWithRank(vector<int> &arr, int N){
+
+        priority_queue<int,vector<int>,greater<int>>pq;
+        
+        for(int i=0;i<arr.size();i++){
+            pq.push(arr[i]);
+        }
+        int j = 0;
+        unordered_map<int,int>m;
+        while(!pq.empty()){
+            int test = pq.top();
+            m[test] = j;
+            pq.pop();
+            while(!pq.empty() && test==pq.top()){
+                m[test] = j;
+                pq.pop();
+            }
+            j++;
+        }
+        
+        vector<int>ans;
+        for(int i=0;i<N;i++){
+            ans.push_back(m[arr[i]]+1);
+        }
+        return ans;
+    }
+
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while(t--){
+        //Input
+
+       int n; cin >> n;
+       vector<int> vec(n);
+       for(int i = 0;i<n;i++) cin >> vec[i];
+
+        Solution obj;
+        vector<int> ans = obj.replaceWithRank(vec,n);
+        for(int i = 0;i<n;i++) cout << ans[i] << " ";
+        cout << endl;
+        
+
+        
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
