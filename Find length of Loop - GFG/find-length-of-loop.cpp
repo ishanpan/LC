d4af029e64f2a4c18789bef73b48001361b456f9
Ishan Pandey <70710170+ishanpan@users.noreply.github.com>
2023-06-18 02:31:53 +0530

    Added solution - LeetHub

---
+++ Find length of Loop - GFG/find-length-of-loop.cpp
@@ -0,0 +1,115 @@
+//{ Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+int countNodesinLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        cout<< countNodesinLoop(head) << endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+//Function to find the length of a loop in the linked list.
+int countNodesinLoop(struct Node *head)
+{
+    // Code here
+    
+    Node* slow = head;
+    Node* fast = head;
+    
+    Node* cycleNode = NULL;
+    
+    
+    while(fast && fast->next){
+        
+        slow = slow->next;
+        fast = fast->next->next;
+        
+        if(fast==slow){
+            slow = head;
+            while(slow!=fast){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            
+            cycleNode = slow;
+            break;
+            
+        }
+        
+    }
+    if(cycleNode==NULL){
+        return 0;
+    }
+    int count = 1;
+    Node* curr = cycleNode->next;
+    while(cycleNode!=curr){
+        curr=curr->next;
+        count++;
+    }
+    return count;
+}
